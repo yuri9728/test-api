aa264d4799921ce37edd6099a3fa0bcf176ca9fc
Yuri <uendeligz@gmail.com>
2023-07-01 18:02:03 +0300

    Add AppDbContext in DI container

--- Test.WebApi/Program.cs
+++ Test.WebApi/Program.cs
@@ -1,9 +1,16 @@
+using Test.Infrastructure.DbContexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var postgresConnectionString = builder.Configuration.GetConnectionString("POSTGRESQL");
+
+builder.Services.AddNpgsql<AppDbContext>(postgresConnectionString, npgsqlOptions
+	=> npgsqlOptions.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName));
 
 var app = builder.Build();
 
