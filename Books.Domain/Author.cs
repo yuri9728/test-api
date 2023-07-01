876ba2e5b41c8d7fca76e8bee4f23666a7ea92f5
Yuri <uendeligz@gmail.com>
2023-07-01 15:50:54 +0300

    Add domain entities (#1)
    * Create domain project
    
    * Add domain entities

---
+++ Books.Domain/Author.cs
@@ -0,0 +1,9 @@
+﻿namespace Books.Domain;
+
+public class Author
+{
+	public Guid Id { get; set; }
+	public string Name { get; set; } = string.Empty;
+
+	public ICollection<Book> Books { get; set; }
+}
---
+++ Books.Domain/User.cs
@@ -0,0 +1,8 @@
+﻿namespace Books.Domain;
+
+public class User
+{
+	public Guid Id { get; set; }
+	public string Login { get; set; } = string.Empty;
+	public string PasswordHash { get; set; } = string.Empty;
+}
