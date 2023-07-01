8b3897f97aafd0a37c100e4fa9a88f6bf2181f17
Yuri <uendeligz@gmail.com>
2023-07-01 17:55:59 +0300

    Create single DB context

---
+++ Test.Infrastructure/DbContexts/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Test.Domain;
+
+namespace Test.Infrastructure.DbContexts;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Book> Books { get; set; }
+    public DbSet<Author> Authors { get; set; }
+}
