476ca85397162cc8316b94d3b017a89520165002
Yuri <uendeligz@gmail.com>
2023-07-01 18:34:31 +0300

    Add endpoint for book creation

--- Test.WebApi/Endpoints/BookEndpoints.cs
+++ Test.WebApi/Endpoints/BookEndpoints.cs
@@ -12,6 +12,9 @@ public static class BookEndpoints
 		var group = builder.MapGroup("/books")
 			.WithTags(nameof(Book) + 's');
 
+		group.MapPost("/", CreateBook)
+			.WithName(nameof(CreateBook));
+
 		group.MapGet("/", GetBooks)
 			.WithName(nameof(GetBooks));
 
@@ -21,6 +24,15 @@ public static class BookEndpoints
 		group.WithOpenApi();
 
 		return builder;
+	}
+
+	private static async Task<Created<Book>> CreateBook(AppDbContext db, Book book)
+	{
+		await db.Books.AddAsync(book);
+
+		await db.SaveChangesAsync();
+
+		return TypedResults.Created($"books/{book.Id}", book);
 	}
 
 	private static async Task<Results<Ok<Book[]>, NoContent>> GetBooks(AppDbContext db)
