4ee1c2541bbe61ddc8709968cbad787a9dd9c7d0
Yuri <uendeligz@gmail.com>
2023-07-10 03:26:55 +0300

    Add authorization configuration

---
+++ Test.WebApi/Configurations/AuthorizationConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Test.WebApi.Configurations;
+
+public static class AuthorizationExtensions
+{
+	public static IServiceCollection ConfigureAuthorization(this IServiceCollection services)
+	{
+		var fallbackPolicy = new AuthorizationPolicyBuilder()
+			.AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+			.RequireAuthenticatedUser()
+			.Build();
+
+		services.AddAuthorization(options =>
+		{
+			options.FallbackPolicy = fallbackPolicy;
+		});
+
+		return services;
+	}
+}
