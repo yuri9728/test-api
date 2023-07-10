30de12ea298b65f8c1e43d5ba3076a82a9c98d24
Yuri <uendeligz@gmail.com>
2023-07-10 03:26:39 +0300

    Add authentication configuration

---
+++ Test.WebApi/Configurations/AuthenticationConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Test.WebApi.Configurations;
+
+public static class AuthenticationConfiguration
+{
+	public static IServiceCollection ConfigureAuthentication(this IServiceCollection services, IConfiguration configuration)
+	{
+		LifetimeValidator lifetimeValidator = new((
+			notBefore,
+			expires,
+			securityToken,
+			validationParameter) => DateTime.UtcNow <= expires);
+
+		string? key = configuration["Secrets:SecurityKey"];
+		ArgumentException.ThrowIfNullOrEmpty(key);
+
+		TokenValidationParameters tokenValidationParameters = new()
+		{
+			ValidateIssuerSigningKey = true,
+			IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key)),
+			ValidateIssuer = true,
+			ValidIssuer = Environment.MachineName,
+			ValidateAudience = false,
+			ValidateLifetime = true,
+			LifetimeValidator = lifetimeValidator
+		};
+
+		services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+		{
+			options.TokenValidationParameters = tokenValidationParameters;
+		});
+
+		return services;
+	}
+}
